03ec4545ce027f7170b12f3b56e2d57546cb5a0e
thomaspj1017 <thomaspj@rose-hulman.edu>
2016-01-13 21:36:14 -0500

    Update neopixel.ino

--- neopixel/neopixel.ino
+++ neopixel/neopixel.ino
@@ -3,13 +3,15 @@ test from Ryan
 #define BUTTON_PIN 2
 #define PIXEL_PIN 6
 #define PIXEL_COUNT  50
-int yellow_block[4] = {48,49,43,44,255,255,0};
-int purble_T[4] = {47,48,49,43,128,0,128};
-int blue_L[4] = {47,42,43,44,0,0,255};
-int orange_L[4] = {49,42,43,44,255,165,0};
-int green_Z[4] = {48,49,42,43,0,255,0};
-int red_Z[4] = {47,48,43,44,255,0,0};
-int cyan_stick = {41,42,43,44,0,255,255};
+int yellow_block[] = {48, 49, 43, 44, 255, 255, 0};
+int purble_T[] = {47, 48, 49, 43, 128, 0, 128};
+int blue_L[] = {47, 42, 43, 44, 0, 0, 255};
+int orange_L[] = {49, 42, 43, 44, 255, 165, 0};
+int green_Z[] = {48, 49, 42, 43, 0, 255, 0};
+int red_Z[] = {47, 48, 43, 44, 255, 0, 0};
+int cyan_stick[] = {41, 42, 43, 44, 0, 255, 255};
+int box[7];
+
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(PIXEL_COUNT, PIXEL_PIN, NEO_GRB + NEO_KHZ800);
 
 bool oldState = HIGH;
@@ -19,32 +21,31 @@ void setup() {
   pinMode(BUTTON_PIN, INPUT_PULLUP);
   pixels.begin();
   pixels.show();
+  for (int i = 0; i < 7; i++) {
+    box[i] = green_Z[i];
+  }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  // create_shape(red_Z);
-}
-
-void create_shape(shape[7]){
-  int c = 0;
-  while (c > 0){
-    for (int i = 0; i<4; i++){
-     pixels.setPixelColor(shape[i], pixels.Color(shape[4],shape[5],shape[6)); 
+  int c = 9;
+  while (c > 0) {
+    for (int i = 0; i < 4; i++) {
+      pixels.setPixelColor(box[i], pixels.Color(box[4], box[5], box[6]));
     }
     pixels.show();
     delay(500);
-    for (int i = 0; i<4; i++){
-     pixels.setPixelColor(shape[i], pixels.Color(0,0,0)); 
+    for (int i = 0; i < 4; i++) {
+      pixels.setPixelColor(box[i], pixels.Color(0, 0, 0));
     }
     pixels.show();
     drop_pixels();
-    c++;
+    c = c - 1;
   }
 }
 
-void drop_pixels(){
-  for (int i = 0; i<4; i++){
-    box[i]=box[i]-5;
+void drop_pixels() {
+  for (int i = 0; i < 4; i++) {
+    box[i] = box[i] - 5;
   }
 }
