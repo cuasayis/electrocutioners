7a4f02d22f605f3ec7da3c36c725b6c511eb2c0e
Ryan Greenlee <ryan.greenlee1@gmail.com>
2016-01-18 18:26:05 -0500

    added comments

--- neopixel/neopixel.ino
+++ neopixel/neopixel.ino
@@ -1,39 +1,167 @@
-// Inlude the Adafruit_NeoPixel library.
+// Include the Adafruit_NeoPixel library
 #include <Adafruit_NeoPixel.h>
 
-// Define constants for pin values.
+// Define constants for pin values
 const int BUTTON_PIN = 2;
 const int PIXEL_PIN = 6;
-const int PIXEL_COUNT  50;
+const int PIXEL_COUNT = 50;
 
 // Define arrays for Tetrimino pieces.
 int yellow_block[] = {48, 49, 43, 44, 255, 255, 0};
-int purble_T[] = {47, 48, 49, 43, 128, 0, 128};
+int purple_T[] = {47, 48, 49, 43, 128, 0, 128};
 int blue_L[] = {47, 42, 43, 44, 0, 0, 255};
 int orange_L[] = {49, 42, 43, 44, 255, 165, 0};
 int green_Z[] = {48, 49, 42, 43, 0, 255, 0};
 int red_Z[] = {47, 48, 43, 44, 255, 0, 0};
-int cyan_stick[] = {41, 42, 43, 44, 0, 255, 255};
+int cyan_stick[] = {46, 47, 48, 49, 0, 255, 255};
 int box[7];
+int game_board[10][5];
+int r;
+int g;
+int b;
+bool oldbuttonState;
 
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(PIXEL_COUNT, PIXEL_PIN, NEO_GRB + NEO_KHZ800);
 
-bool oldState = HIGH;
 int showType = 0;
+
 void setup() {
-  // put your setup code here, to run once:
   pinMode(BUTTON_PIN, INPUT_PULLUP);
   pixels.begin();
   pixels.show();
-  for (int i = 0; i < 7; i++) {
-    box[i] = green_Z[i];
+  for (int i = 0; i < 10; i++) {
+    for (int j = 0; j < 5; j++) {
+      int rn = random(1,8);
+      game_board[i][j] = rn;
+    }
   }
+  //color_board();
+  oldbuttonState = HIGH;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  int c = 9;
-  while (c > 0) {
+  //drop_shape();
+  //color_board();
+  //random_drop();
+  button_push();
+}
+
+void color_board() {
+  for (int i = 0; i < 10; i++) {
+    for (int j = 0; j < 5; j++) {
+      if (game_board[i][j] == 0) {
+        r = 0;
+        g = 0;
+        b = 0;
+      }
+      else if (game_board[i][j] == 1) {      // yellow
+        r = 255;
+        g = 255;
+        b = 0;
+      }
+      else if (game_board[i][j] == 2) {      // purple
+        r = 128;
+        g = 0;
+        b = 128;
+      }
+      else if (game_board[i][j] == 3) {      // blue
+        r = 0;
+        g = 0;
+        b = 255;
+      }
+      else if (game_board[i][j] == 4) {      // orange
+        r = 255;
+        g = 165;
+        b = 0;
+      }
+      else if (game_board[i][j] == 5) {      // green
+        r = 0;
+        g = 255;
+        b = 0;
+      }
+      else if (game_board[i][j] == 6) {      // red
+        r = 255;
+        g = 0;
+        b = 0;
+      }
+      else if (game_board[i][j] == 7) {     // cyan
+        r = 0;
+        g = 255;
+        b = 255;
+      }
+      int pos = 5 * i + j;
+      pixels.setPixelColor(pos, pixels.Color(r, g, b));
+    }
+  }
+  pixels.show();
+}
+
+void random_drop(){
+  int rn = random(1,8);
+  switch (rn){
+    case 1:
+    for (int i = 0; i < 7; i++) {
+      box[i] = yellow_block[i];
+    }
+    break;
+    case 2:
+    for (int i = 0; i < 7; i++) {
+      box[i] = purple_T[i];
+    }
+    break;
+    case 3:
+    for (int i = 0; i < 7; i++) {
+      box[i] = blue_L[i];
+    }
+    break;
+    case 4:
+    for (int i = 0; i < 7; i++) {
+      box[i] = orange_L[i];
+    }
+    break;
+    case 5:
+    for (int i = 0; i < 7; i++) {
+      box[i] = green_Z[i];
+    }
+    break;
+    case 6:
+    for (int i = 0; i < 7; i++) {
+      box[i] = red_Z[i];
+    }
+    break;
+    case 7:
+    for (int i = 0; i < 7; i++) {
+      box[i] = cyan_stick[i];
+    }
+    break;
+  }
+  drop_shape();
+}
+
+void button_push(){
+  int buttonState = digitalRead(2);
+  if (buttonState == LOW && oldbuttonState == HIGH)
+  {
+    random_drop();
+    oldbuttonState = LOW;
+  }
+  if (buttonState == HIGH){
+    oldbuttonState = HIGH;
+  }
+}
+
+void drop_pixels() {
+  for (int i = 0; i < 4; i++) {
+    box[i] = box[i] - 5;
+  }
+}
+
+void drop_shape() {
+  int c;
+  //for (int i = 0; i < 7; i++) {
+  //  box[i] = purple_T[i];
+  //}
+  for (c = 0; c < 10; c++) {
     for (int i = 0; i < 4; i++) {
       pixels.setPixelColor(box[i], pixels.Color(box[4], box[5], box[6]));
     }
@@ -44,12 +172,5 @@ void loop() {
     }
     pixels.show();
     drop_pixels();
-    c = c - 1;
-  }
-}
-
-void drop_pixels() {
-  for (int i = 0; i < 4; i++) {
-    box[i] = box[i] - 5;
   }
 }
